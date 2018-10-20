c73be38f4c417bc215ce621a1f94b2ddaea653c1
Joel Pierre <jpp@jppdesigns.co.uk>
2018-10-20 13:26:30 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp'),
+  prefix = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  livereload = require('gulp-livereload'),
+  plumber = require('gulp-plumber');
+
+// Compress SCSS Task -
+gulp.task('sass', () => {
+  gulp.src('assets/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'nested' // nested, expanded, compact, compressed
+    }))
+    .pipe(plumber.stop())
+    .pipe(prefix({
+      env: './.browserslistrc',
+      grid: true,
+      flexbox: true
+    }))
+    .pipe(gulp.dest('dist/'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+
+  livereload.listen();
+
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass',  'watch']);
